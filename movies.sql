273b8b2c6edc9dc171276dc9cdc9a4b4d9697e66
unknown <zbyljacar08@gmail.com>
2025-06-05 13:28:59 +0800

    Added movies for practice

---
+++ movies.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE Practice;
+CREATE TABLE Movies (title VARCHAR(50), genre VARCHAR(50), year INT, rating DECIMAL);
+INSERT INTO Movies (title, genre, year, rating) VALUES ("inception", "Scifi", 2010, 8.8)
+
